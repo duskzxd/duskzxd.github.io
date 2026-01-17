dbce9b169bada9672193b836c942ce3b91e33239
duskz <du.bist.inaktiv@gmail.com>
2026-01-17 08:59:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const announcement = document.getElementById("announcement");
 const closeBtn = document.getElementById("closeAnnouncement");
 
-const SHOW_ANNOUNCEMENT = true;
-
-if (SHOW_ANNOUNCEMENT && !localStorage.getItem("announcementClosed")) {
+if (!localStorage.getItem("announcementClosed")) {
   announcement.classList.remove("hidden");
 }
 
-closeBtn?.addEventListener("click", () => {
+closeBtn.onclick = () => {
   announcement.classList.add("hidden");
   localStorage.setItem("announcementClosed", "true");
-});
+};
+
